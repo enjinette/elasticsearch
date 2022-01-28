7e19f60fb11171cd2468546c73ed83af6873fa23
enjinette <jnetabayon@gmail.com>
2022-01-28 13:09:51 +0800

    Update with comments

--- flaskelastic.py
+++ flaskelastic.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 from flask import Flask, json, request, send_file
 from elasticsearch import Elasticsearch
 from datetime import datetime
@@ -8,15 +9,19 @@ api = Flask(__name__)
 
 @api.route('/read', methods=['GET'])
 def get_read():
+  # Retrieve parameters from the request
   index = request.args.get('index')
   size = request.args.get('size')
   try:
+    # Connect to Elasticsearch
     elastic_client = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    # Search from elasticsearch by index and size
     resp_search = elastic_client.search(index=index, query={"match_all": {}}, size=size)
     result = {
       'size': len(resp_search["hits"]["hits"])
     }
     result['data'] = []
+    # Append all found data to a list
     for hit in resp_search['hits']['hits']:
       result['data'].append(hit)
     return json.dumps(result)
@@ -25,20 +30,26 @@ def get_read():
 
 @api.route('/start_read_all', methods=['GET'])
 def get_start_read_all():
+  # Retrieve parameter from the request
   index = request.args.get('index')
   try:
+    # Connect to Elasticsearch
     elastic_client = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    # Search from elasticsearch by index
     resp_search = elastic_client.search(index=index, query={"match_all": {}})
+    # Set CSV filename based on datetime and given index
     csv_dir  = './csv/'
     csv_filename = '{timestamp}_{filename}'.format(filename=index, timestamp=datetime.now().strftime("%Y%m%d%H%M%S"))
     csv_file = csv_filename + '.csv'
     csv_path = os.path.join(csv_dir, csv_file)
     csv_column = resp_search['hits']['hits'][0].keys()
+    # Write data to a csv file
     with open(csv_path, 'w+') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=csv_column)
         writer.writeheader()
         for data in resp_search['hits']['hits']:
             writer.writerow(data)
+    # Set response data
     result = {
       'job_id': csv_filename
     }
@@ -48,13 +59,15 @@ def get_start_read_all():
 
 @api.route('/read_all', methods=['GET'])
 def get_read_all():
+  # Retrieve parameter from the request
   job_id = request.args.get('job_id')
   try:
-    csv_dir  = "./csv"
-    csv_file = "{filename}.csv".format(filename=job_id)
+    # Set CSV filename based on job_id parameter
+    csv_dir  = './csv'
+    csv_file = '{filename}.csv'.format(filename=job_id)
     csv_path = os.path.join(csv_dir, csv_file)
     if not os.path.isfile(csv_path):
-        return "File not found: file %s was not found on the server" % csv_file
+        return 'File not found: file {filename} was not found on the server'.format(filename=csv_file)
     return send_file(csv_path, as_attachment=True, attachment_filename=csv_file)
   except Exception as err:
     return 'Error job_id not found'
